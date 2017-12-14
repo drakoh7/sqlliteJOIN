a2ab9c7e85c72a0dfc0f1570bfae023725d837e6
drakoh7 <drakoh7@mail.ru>
2017-12-14 17:42:17 +0500

    Начало

---
+++ src/main/java/ru/alexandr/pc/p0371_sqliteinnerjoin/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.alexandr.pc.p0371_sqliteinnerjoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
